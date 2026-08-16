0348e1cb0e77513d6808730c6d49b018ea86d4ae
contactadammk-AMK <contactadammk@gmail.com>
2026-08-16 13:03:16 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,10 @@
 const CACHE_NAME = 'kudina-shell-v1';
-const APP_SHELL = ['/app', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
-
+const APP_SHELL = [
+  '/app',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
